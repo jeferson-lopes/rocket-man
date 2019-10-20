b54d98e12192b692949bbdc5de05299abb52f94e
JefersonLopes1 <JefersonLopes1>
2019-10-20 13:57:25 -0300

    ajustes

--- js/bolas.js
+++ js/bolas.js
@@ -1,7 +1,16 @@
-setInterval(function () {
+/**setInterval(function () {
     var bolaRoxa = document.querySelector(".bolaRoxa");
     bolaRoxa.classList.add("aumentaBola1")
     setTimeout(function () {
         bolaRoxa.classList.remove("aumentaBola1")
     }, 1000)
 }, 1100)
+
+setInterval(function () {
+    var bolaAzul = document.querySelector(".bolaAzul");
+    bolaAzul.classList.add("aumentaBola1")
+    setTimeout(function () {
+        bolaAzul.classList.remove("aumentaBola1")
+    }, 1000)
+}, 1100)
+*/
